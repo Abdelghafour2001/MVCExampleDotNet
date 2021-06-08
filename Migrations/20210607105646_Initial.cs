c7541f0f968a62b1737e1b05f49c8f82c9f6d478
Abdelghafour2001 <61706790+Abdelghafour2001@users.noreply.github.com>
2021-06-08 16:50:14 +0100

    Add files via upload

---
+++ Migrations/20210607105646_Initial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVC.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personnes",
+                columns: table => new
+                {
+                    Nom = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personnes", x => x.Nom);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Personnes");
+        }
+    }
+}
---
+++ Migrations/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVC.Data;
+
+namespace MVC.Migrations
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Personne> Personnes{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=abdel_test;Integrated Security=true;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personne>()
+                .HasKey(e => e.Nom);
+                
+        }
+    }
+}
